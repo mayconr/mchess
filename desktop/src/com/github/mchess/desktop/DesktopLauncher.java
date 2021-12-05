08d17588e9eb631e556b61fdee30b36f457f6147
mayconr <maycon.greg@gmail.com>
2021-12-05 10:37:06 -0300

    initial commit

---
+++ desktop/src/com/github/mchess/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.mchess.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.mchess.MChessGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MChessGame(), config);
+	}
+}
